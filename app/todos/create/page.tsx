1abe3e67d9d53c5031902a3b22133d06b4acb253
G Tavares <tavares.gabriel.dev@gmail.com>
2026-05-05 17:33:32 -0300

    feat: add Prisma setup with SQLite and Todo model
    - Updated package.json to include Prisma and @prisma/client dependencies.
    - Created prisma.config.ts for Prisma configuration.
    - Added migration SQL file to create Todo table with fields: id, titulo, descricao, status, createdAt, and updatedAt.
    - Introduced migration_lock.toml for migration tracking.
    - Defined Prisma schema in schema.prisma with Todo model and its attributes.

---
+++ app/todos/create/page.tsx
@@ -0,0 +1,7 @@
+export default function CreateTodo() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center ">
+      <h1 className="text-2xl font-bold">Todo</h1>
+    </div>
+  );
+}
